52c13f550deb1d429789c8c8d3826aa70f1b6fa5
Nelson David <nedsanchezm@udistrital.edu.co>
2023-02-08 18:50:09 -0500

    confi el cod

--- taller 2.py
+++ taller 2.py
@@ -1,4 +1,7 @@
-def.imprimir (n):
-for i in range (1,n):
-print(i)
-    
+n=int(input( "Ingrese el numero: ") )
+i= 0
+def imprimir (n):
+    for i in  range (1,n):
+        print(i)
+        i+= 1
+        
